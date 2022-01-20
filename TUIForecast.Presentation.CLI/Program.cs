3e63428d631775ded60c5e673a37c83f89f2fb97
Ferran Ramirez <ferran@notguiri.com>
2022-01-20 16:20:41 +0100

    Set up DI in CLI project

---
+++ TUIForecast.Presentation.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using TUIForecast.Application.Contract;
+
+namespace TUIForecast.Presentation.CLI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var startup = new Startup();
+        }
+    }
+}
---
+++ TUIForecast.Presentation.CLI/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using TUIForecast.Application.Contract;
+using TUIForecast.Application.Impl;
+using System;
+using TUIForecast.Application.Contract.RequestServices;
+using TUIForecast.Application.Impl.RequestServices;
+
+namespace TUIForecast.Presentation.CLI
+{
+    public class Startup
+    {
+        private readonly IServiceProvider provider;
+        public IServiceProvider Provider => provider;
+
+        public Startup()
+        {
+            provider =
+                new ServiceCollection()
+                  .AddSingleton<HttpClient>()
+                  .AddSingleton<ICityRequestService, CityRequestService>()
+                  .AddSingleton<IWeatherRequestService, WeatherRequestService>()
+                  .AddSingleton<IForecastService, ForecastService>()
+                  .BuildServiceProvider();
+        }
+    }
+}
